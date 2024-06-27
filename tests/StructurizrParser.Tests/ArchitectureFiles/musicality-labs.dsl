3e4e11c14b8cd5e7387da6d9f98f1bb04dac7440
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2024-06-27 21:41:28 +0300

    Tests for Structurizr JSON parser were added

---
+++ tests/StructurizrParser.Tests/ArchitectureFiles/musicality-labs.dsl
@@ -0,0 +1,69 @@
+workspace {
+
+    model {
+        
+        changesMQ = element "musicality_labs.data.changes" "RabbitMQ" {
+            tags "MQ" 
+        }
+        
+        storage = softwareSystem "Storage" {
+            storageWorker = container "Storage Worker" {
+                tags "Worker" 
+				technology ".NET Core"
+				url "https://github.com/MusicalityLabs/musicality-labs-storage-worker"
+            }
+            storageApi = container "Storage Api" {
+                tags "WebApi" 
+				technology ".NET Core"
+				url "https://github.com/MusicalityLabs/musicality-labs-storage-api"
+				
+                storageWorker -> this "Changes" "REST" "Sync"
+            }
+			storageDatabase = container "storageapi" {
+                tags "Database" 
+				technology "PostgreSQL"
+				
+                storageApi -> this "Changes" "SQL" "Sync"
+            }
+        }
+                       
+        changesMQ -> storageWorker "Changes" "AMQP" "Async"
+    }
+
+    views {
+        systemLandscape {
+            include * 
+			autolayout lr
+        }
+		
+		container storage {
+            include *
+            autolayout lr
+        }
+		        
+        styles {
+            element "ExternalSystem" {
+                background #7f7f7f
+            }
+            element "Database" {
+                shape cylinder
+            }
+            element "MQ" {
+                shape pipe
+            }
+            relationship "Sync" {
+                dashed false
+            }
+			relationship "Async" {
+                dashed true
+            }
+			relationship "TechDebt" {
+				color red
+			}
+        }
+       
+        theme default
+
+    }
+    
+}
---
+++ tests/StructurizrParser.Tests/JsonParserTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Byndyusoft.ArchitectureTesting.StructurizrParser.Tests
+{
+    using System;
+    using System.IO;
+    using Abstractions.ServiceContracts;
+    using Abstractions.ServiceContracts.Dependencies;
+    using FluentAssertions;
+    using Xunit;
+
+    public class StructurizrJsonParsingTestCase
+    {
+        public string JsonFileName { get; set; }
+
+        public Func<string, bool> ServiceNameMatcher { get; set; }
+
+        public ServiceContract[] ExpectedServiceContracts { get; set; }
+    }
+
+    public class JsonParserTests
+    {
+        public static readonly TheoryData<StructurizrJsonParsingTestCase> StructurizrJsonParsingTestCases;
+
+        static JsonParserTests()
+        {
+            StructurizrJsonParsingTestCases
+                = new TheoryData<StructurizrJsonParsingTestCase>
+                  {
+                      new StructurizrJsonParsingTestCase
+                      {
+                          JsonFileName = @".\ArchitectureFiles\musicality-labs.json",
+                          ServiceNameMatcher = x => x.StartsWith("musicality-labs", StringComparison.InvariantCultureIgnoreCase),
+                          ExpectedServiceContracts
+                              = new[]
+                                {
+                                    new ServiceContract
+                                    {
+                                        Name = "musicality-labs-storage-api",
+                                        Dependencies
+                                            = new DependencyBase[]
+                                              {
+                                                  new DbDependency {Name = "storageapi"}
+                                              }
+                                    },
+                                    new ServiceContract
+                                    {
+                                        Name = "musicality-labs-storage-worker",
+                                        Dependencies
+                                            = new DependencyBase[]
+                                              {
+                                                  new RabbitDependency {Name = "musicality_labs.data.changes", Direction = MqDependencyDirection.Incoming},
+                                                  new ApiDependency {Name = "musicality-labs-storage-api"}
+                                              }
+                                    }
+                                }
+                      }
+                  };
+        }
+
+        [Theory]
+        [MemberData(nameof(StructurizrJsonParsingTestCases))]
+        public void ShouldParseStructurizrJson(StructurizrJsonParsingTestCase testCase)
+        {
+            // Given
+            var jsonString = File.ReadAllText(testCase.JsonFileName);
+            var parser = new JsonParser(testCase.ServiceNameMatcher);
+           
+            // When
+            var actualServiceContracts = parser.Parse(jsonString);
+
+            // Then
+            actualServiceContracts.Should().BeEquivalentTo(testCase.ExpectedServiceContracts);
+        }
+    }
+}
