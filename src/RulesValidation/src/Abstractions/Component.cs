f7acf87184b49e0162a850a1091325939310088b
Litichevskiy Dmitriy <litichevskiydv@gmail.com>
2026-03-04 10:52:50 +0300

    Components structure was described

---
+++ src/RulesValidation/src/Abstractions/Component.cs
@@ -0,0 +1,25 @@
+﻿namespace Byndyusoft.ArchitectureTesting.RulesValidation.Abstractions
+{
+    /// <summary>
+    ///     Компонент, из которых состоит приложение
+    /// </summary>
+    public class Component
+    {
+        /// <summary>
+        ///     Путь до компонента в графе
+        /// </summary>
+        public ComponentsGraphPath Path { get; set; }
+
+        /// <summary>
+        ///     Компоненты, являющиеся непосредственным частями данного
+        /// </summary>
+        public Component[] Children { get; set; }
+
+        /// <summary>
+        ///     Компоненты, от которых зависит данный
+        /// </summary>
+        public Component[] Dependencies { get; set; }
+
+        public override string ToString() => Path.Text;
+    }
+}
---
+++ src/RulesValidation/src/Abstractions/ComponentsGraphPath.cs
@@ -0,0 +1,50 @@
+﻿namespace Byndyusoft.ArchitectureTesting.RulesValidation.Abstractions
+{
+    using System;
+
+    /// <summary>
+    ///     Путь в графе компонентов
+    /// </summary>
+    public class ComponentsGraphPath
+    {
+        /// <summary>
+        ///     Инициализация зависимостей объекта
+        /// </summary>
+        /// <param name="segments">Сегменты пути в графе компонентов</param>
+        public ComponentsGraphPath(string[] segments)
+        {
+            Segments = segments;
+            Text = string.Join(".", segments);
+        }
+
+        /// <summary>
+        ///     Сегменты, из которых состоит путь в графе
+        /// </summary>
+        public string[] Segments { get; }
+
+        /// <summary>
+        ///     Путь в графе в виде одной строки
+        /// </summary>
+        public string Text { get; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+
+            return Equals((ComponentsGraphPath)obj);
+        }
+
+        public override int GetHashCode()
+            => StringComparer.InvariantCultureIgnoreCase.GetHashCode(Text);
+
+        public override string ToString() => Text;
+
+        private bool Equals(ComponentsGraphPath other)
+            => string.Equals(Text, other.Text, StringComparison.InvariantCultureIgnoreCase);
+    }
+}
